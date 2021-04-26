30706386d4c7dd0d213cd9e233b02b92cf60f355
Patipark Sopha <patipark@profile.co.th>
2021-04-26 08:30:07 +0700

    แสดง bootstrap-icon เครื่องหมาย check ok

--- pages/success.js
+++ pages/success.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { Container, Row, Col, Button } from 'react-bootstrap'
-
+import { CheckCircleFill } from 'react-bootstrap-icons'
 export default function Success () {
 
   return (
@@ -11,6 +11,7 @@ export default function Success () {
       <Container fluid="md" className="mt-5 p-2">
         <Row className="justify-content-md-center mb-2">
           <Col className="text-center">
+            <CheckCircleFill color="green" size={96} />
             <h3>Thankyou</h3>
             <p>Login success</p>
           </Col>
